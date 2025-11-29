50048a793909a7ac1bd5fcff18c8149a5ea93c2c
Lee Jia Yi <leejiayi16@gmail.com>
2025-11-29 22:01:09 +0800

    Moved code from DataHandler to myfirstscala

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -1,10 +1,41 @@
-import scalafx.application.JFXApp3
-import scalafx.application.JFXApp3.PrimaryStage
+import scala.io.Source
 
-object MyApp extends JFXApp3:
+case class HotelBooking(
+                         originCountry: String,
+                         hotelName: String,
+                         noOfPeople: Int,
+                         bookingPrice: Double,
+                         discount: String,
+                         profitMargin: Double
+                       )
 
-  override def start(): Unit =
-    stage = new PrimaryStage()
+object DataHandler {
 
+  // function to read CSV file
+  def loadData(filePath: String): List[Array[String]] = {
+    val bufferedSource = Source.fromFile(filePath, "Windows-1252") // specify encoding
+    val data = bufferedSource.getLines().map(_.split(",")).toList
+    bufferedSource.close()
+    data
+  }
 
-end MyApp
+  // Function to show first 5 rows
+  def previewData(data: List[Array[String]]): Unit = {
+    println("Showing first 5 rows:")
+    data.take(5).foreach(row => println(row.mkString(" | ")))
+  }
+
+  // Function to count rows
+  def countRows(data: List[Array[String]]): Int = {
+    data.length
+  }
+
+  def main(args: Array[String]): Unit = {
+    val filePath = "Hotel_Dataset.csv"
+
+    val data = loadData(filePath)
+
+    previewData(data)
+    println(s"Total rows: ${countRows(data)}")
+  }
+}
