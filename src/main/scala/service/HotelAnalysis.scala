9ad35f9dbd07af6b79cc8128b990e28b0d884052
Lee Jia Yi <leejiayi16@gmail.com>
2025-12-06 00:04:28 +0800

    Update HotelAnalysis to new logic

--- src/main/scala/service/HotelAnalysis.scala
+++ src/main/scala/service/HotelAnalysis.scala
@@ -1,72 +1,116 @@
 package service
 
-import model.Booking
+import model.*
+
+import scala.language.postfixOps
+import scala.util.Try
 
 class HotelAnalysis(bookings: List[Booking]) {
-
-  private def analyzeGroups[K](
-                                grouper: Booking => K,
-                                aggregator: List[Booking] => Double,
-                                selector: Iterable[(K, Double)] => (K, Double)
-                              ): (K, Double) = {
-    val grouped = bookings.groupBy(grouper)
-    val scored = grouped.map { case (key, list) =>
-      (key, aggregator(list))
+  
+  // Grouping Hotels by Country-Name-City
+  private def groupHotels(bookings: List[Booking]): Map[Hotel, List[Booking]] = {
+    bookings.groupBy { b =>
+      Hotel(b.destinationCountry, b.hotelName, b.destinationCity)
     }
-    selector(scored)
   }
-
+  
+  // Normalisation when high score = high value 
+  private def normalize(value: Double, min: Double, max: Double): Double = {
+    if(max == min)
+      100.0
+    else
+      ((value - min) / (max - min)) * 100
+  }
+  
+  // Normalisation when low score = high value
+  private def normalizeInverted(value: Double, min: Double, max: Double): Double = {
+    if (max == min)
+      100.0
+    else
+      1 - ((value - max) / (max - min)) * 100
+  }
+  
   // Helper for Average calculation to keep code clean
   private def avg(nums: List[Double]): Double =
     if (nums.isEmpty) 0.0 else nums.sum / nums.size
-
+  
   private def getMostFrequent[T](data: List[T], extractor: T => String): (String, Int) = {
     data.groupBy(extractor)
       .map { case (key, group) => (key, group.size) }
       .maxBy(_._2)
   }
 
+  // -------------------------------------------
   // Q1: Highest number of bookings
+  // -------------------------------------------
   def getTopCountryWithMostBookings(): (String, Int) = {
-    getMostFrequent(bookings, _.originCountry)
+    getMostFrequent(bookings, _.destinationCountry)
   }
 
-  // Q2a: Most economical hotel (by Booking Price)
-  def getMostEconomicalByPrice(): (String, Double) = {
-    analyzeGroups(
-      grouper = _.hotelName,
-      aggregator = list => avg(list.map(_.bookingPrice)),
-      selector = _.minBy(_._2)
-    )
+  // -------------------------------------------
+  // Q2: Most Economical to Customers
+  // -------------------------------------------
+  def getMostEconomicalHotel(bookings: List[Booking]): Option[(HotelEconomicScore, Double)] = {
+    val grouped = groupHotels(bookings)
+
+    val rawStats = grouped.map { case (key, records) =>
+      val avgPrice = records.map(_.bookingPrice).sum / records.size
+      val avgDiscount = records.map(_.discount).sum / records.size
+      val avgMargin = records.map(_.profitMargin).sum / records.size
+      (key, avgPrice, avgDiscount, avgMargin)
+    }.toList
+
+    if (rawStats.isEmpty) return None
+
+    val prices = rawStats.map(_._2)
+    val discounts = rawStats.map(_._3)
+    val margins = rawStats.map(_._4)
+
+    val (minP, maxP) = (prices.min, prices.max)
+    val (minD, maxD) = (discounts.min, discounts.max)
+    val (minM, maxM) = (margins.min, margins.max)
+
+    val scores = rawStats.map { case (key, p, d, m) =>
+      HotelEconomicScore(
+        hotel = key,
+        priceScore = normalizeInverted(p, minP, maxP),
+        discountScore = normalize(d, minD, maxD),
+        marginScore = normalizeInverted(m, minM, maxM)
+      )
+    }
+    val bestHotel = scores.maxBy(_.score)
+    Some(bestHotel, bestHotel.score)
   }
 
-  // Q2b: Most economical hotel (by Highest Discount)
-  def getMostEconomicalByDiscount(): (String, Double) = {
-    analyzeGroups(
-      grouper = _.hotelName,
-      aggregator = list => avg(list.map(_.discount)),
-      selector = _.maxBy(_._2)
-    )
-  }
+  // -------------------------------------------
+  // Q3: Most Profitable Hotel 
+  // -------------------------------------------
+  def getMostProfitableHotel(bookings: List[Booking]): Option[(HotelProfitScore, Double)] = {
+    val grouped = groupHotels(bookings)
 
-  // Q2c: Most economical hotel (by Lowest Profit Margin)
-  def getMostEconomicalByMargin(): (String, Double) = {
-    analyzeGroups(
-      grouper = _.hotelName,
-      aggregator = list => avg(list.map(_.profitMargin)),
-      selector = _.minBy(_._2)
-    )
-  }
+    val rawStats = grouped.map { case (key, records) =>
+      val totalVisitors = records.map(_.noOfPeople).sum.toDouble
+      val avgMargin = records.map(_.profitMargin).sum / records.size
+      (key, totalVisitors, avgMargin)
+    }.toList
 
-  // Q3: Most profitable hotel
-  // Calculation: Sum of (Booking Price * Profit Margin) for all bookings of that hotel
-  def getMostProfitableHotel(): (String, Double, Int) = {
-    bookings.groupBy(_.hotelName)
-      .map { case (hotel, list) =>
-        val totalProfit = list.map(b => b.bookingPrice * b.profitMargin * b.noOfPeople).sum
-        val totalVisitors = list.map(_.noOfPeople).sum
-        (hotel, totalProfit, totalVisitors)
-      }
-      .maxBy(_._2)
+    if (rawStats.isEmpty) return None
+
+    val visitors = rawStats.map(_._2)
+    val margins = rawStats.map(_._3)
+
+    val (minV, maxV) = (visitors.min, visitors.max)
+    val (minM, maxM) = (margins.min, margins.max)
+
+    val scores = rawStats.map { case (key, v, m) =>
+      HotelProfitScore(
+        hotel = key,
+        visitorScore = normalize(v, minV, maxV),
+        marginScore = normalize(m, minM, maxM)
+      )
+    }
+
+    val bestHotel = scores.maxBy(_.score)
+    Some(bestHotel, bestHotel.score)
   }
 }
