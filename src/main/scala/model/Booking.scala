cf5655e761d0771637672efd5e7c656937aafec5
Lee Jia Yi <leejiayi16@gmail.com>
2025-12-05 21:25:30 +0800

    Update Booking.scala
    To accommodate for new logic

--- src/main/scala/model/Booking.scala
+++ src/main/scala/model/Booking.scala
@@ -2,14 +2,36 @@ package model
 
 case class Booking (
                       bookingId: String,
-                      originCountry: String,
+                      destinationCountry: String,
                       hotelName: String,
                       hotelRating: Double,
                       noOfPeople: Int,
+                      noOfDays: Int,
+                      rooms: Int,
                       bookingPrice: Double,
                       discount: Double,
                       profitMargin: Double
                     )
+
+// Group Bookings by country - hotel name - city as one city
+case class Hotel(country: String, name: String, city: String) {
+  override def toString: String = s"$country - $name - $city"
+}
+
+// Scoring for each category
+trait Scorable {
+  def score: Double
+}
+
+// Answer for Q2 is a value
+case class HotelEconomicScore(hotel: Hotel, priceScore: Double, discountScore: Double, marginScore: Double) extends Scorable {
+  override def score: Double = (priceScore + discountScore + marginScore) / 3.0
+}
+
+// Answer for Q3 is a value
+case class HotelProfitScore(hotel: Hotel, visitorScore: Double, marginScore: Double) extends Scorable {
+  override def score: Double = (visitorScore + marginScore) / 2.0
+}
 
 object Booking {
 //  Method to create a HotelBooking from csv
@@ -26,6 +48,8 @@ object Booking {
         hotelName = cols(16),
         hotelRating = cols(17).toDouble,
         noOfPeople = cols(11).toInt,
+        noOfDays = cols(13).toInt,
+        rooms = cols(15).toInt,
         bookingPrice = cols(20).toDouble,
         discount = parsePercentage(cols(21)),
         profitMargin = cols(23).toDouble
